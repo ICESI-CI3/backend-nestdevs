e93add1f11c60ab5116eea672f2c1d77111bbd63
Cristian Perafan <1110282139@u.icesi.edu.co>
2024-04-21 09:45:10 -0500

    Feat: protect routes with authorization based in roles

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, HttpCode, Post, Param, Body, Put, Delete, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Param, Body, Put, Delete, ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductsService } from './product.service';
 import { Product } from './model/product';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Roles } from 'src/auth/decorators/role-auth.decorator';
+import { User, UserRole } from 'src/user/entities/user.entity';
 
 @Controller('products')
 export class ProductController {
@@ -11,27 +14,40 @@ export class ProductController {
         private readonly productService: ProductsService
     ){}
 
+
+    @UseGuards(JwtAuthGuard)
+    @Roles(UserRole.ADMIN,UserRole.SELLER,UserRole.BUYER)
     @Get()
     findAll() {
         return this.productService.findAll();
     }
 
+
+    @UseGuards(JwtAuthGuard)
+    @Roles(UserRole.ADMIN,UserRole.SELLER,UserRole.BUYER)
     @Get(':id')
     findOne(@Param('id', ParseUUIDPipe) id: string): Product {
         return this.productService.findOneById(id);
     }
 
+    @UseGuards(JwtAuthGuard)
+    @Roles(UserRole.ADMIN,UserRole.SELLER)
     @Post()
     @HttpCode(201)
     create(@Body() createProductDto: CreateProductDto) {
         return this.productService.create(createProductDto);
     }
 
+    @UseGuards(JwtAuthGuard)
+    @Roles(UserRole.ADMIN,UserRole.SELLER)
     @Put(':id')
     update(@Param('id', ParseUUIDPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
         return this.productService.update(id, updateProductDto);
     }
 
+
+    @UseGuards(JwtAuthGuard)
+    @Roles(UserRole.ADMIN,UserRole.SELLER)
     @Delete(':id')
     delete(@Param('id', ParseUUIDPipe) id: string) {
         return this.productService.delete(id);
