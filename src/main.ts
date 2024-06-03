c4662f71e82bdd61c162ec9507ba252ae6e04161
CristianPerafan <1110282139@u.icesi.edu.co>
2024-06-02 23:14:00 -0500

    Update cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors({
-    origin: 'http://localhost:3000', 
-    credentials: true, 
-  }));
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(5000);
 }
 bootstrap();
+
