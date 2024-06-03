8a13ef50f9ca0cc5598312db6a8d85595da9eb20
CristianPerafan <1110282139@u.icesi.edu.co>
2024-06-03 09:21:52 -0500

    Update cors configuration

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://icesi-marketplace-frontend.vercel.app',
+    origin: ['https://icesi-marketplace-frontend.vercel.app', 'http://localhost:3000'],
     credentials: true,
+    methods: ["GET", "POST"]
   });
   await app.listen(5000);
 }
