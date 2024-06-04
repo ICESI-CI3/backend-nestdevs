574b48ebbe07de8246e2a465c04f8d062d00a0fc
CristianPerafan <1110282139@u.icesi.edu.co>
2024-06-04 01:05:29 -0500

    Update cors configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['https://icesi-marketplace-frontend.vercel.app', 'http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   });
